831812eb120df63b3600e05b9389264828060203
Lucas Frérot <lucas.frerot@gmail.com>
2020-11-24 11:38:22 -0500

    cleaned up useless functions

--- functions/monifuck.fish
+++
@@ -1,5 +0,0 @@
-function monifuck
-  xrandr --output DP-1-2 --off
-  sleep 5
-  xrandr --output DP-1-2 --auto --right-of DP-1-1
-end
--- functions/workon.fish
+++
@@ -1,5 +0,0 @@
-function workon
-  echo $args[1]
-  echo "Activating virtualenv $args[1]"
-  source $VENVS/$args[1]/bin/activate.fish
-end
