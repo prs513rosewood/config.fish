51e474355bec11777d10320c0050d5df23c6c1e9
Lucas Frérot <lucas.frerot@imtek.uni-freiburg.de>
2023-04-27 09:49:43 +0200

    update

--- conf.d/01_user_vars.fish
+++ conf.d/01_user_vars.fish
@@ -9,6 +9,8 @@ set -x PATH          \
   /usr/games         \
   /usr/local/games
 
+set local $HOME/.local
+
 # User environment variables
 set -x EDITOR (command -v vim)
 set -x VENVS $local/share/virtualenvs
@@ -16,15 +18,16 @@ set -x PASSWORD_STORE_ENABLE_EXTENSIONS "true"
 set -x LESS "-RX"
 
 # Locale variables
-set -x LANG "C.UTF-8"      # fallback if undefined
-set -x LANGUAGE "C.UTF-8"  # english for command output
-# Note: avoid setting LC_ALL, as it overrides
-# everything, making things hard to debug
+## Note: avoid setting LC_ALL, as it overrides
+## everything, making things hard to debug
+set -x LANG       "C.UTF-8"  # fallback if undefined
+set -x LANGUAGE   "C.UTF-8"  # english for command output
+set -x LC_NUMERIC "C.UTF-8"  # dot for numbers
 
 # User paths
 set local $HOME/.local
 set -x PATH $local/bin $PATH
-set -x LD_LIBRARY_PATH "$local/lib:$LD_LIBRARY_PATH"
+set -x LD_LIBRARY_PATH "$local/lib:/usr/local/lib:$LD_LIBRARY_PATH"
 
 # Erase any previously set PYTHONPATH
 set -e PYTHONPATH
@@ -34,3 +37,10 @@ set -x SCONSFLAGS -j\ (math (nproc)/2)
 
 # TeXLive config
 set -x TEXMFHOME $HOME/.config/texmf
+
+# Add /usr/local/lib/python{version}/site-packages
+set python_version (python3 -V | cut -d " " -f 2 | cut -d '.' -f 1,2)
+set -x PYTHONPATH "$local/lib/python$python_version/site-packages:/usr/local/lib/python$python_version/site-packages"
+
+# Set default snakemake profile
+set -x SNAKEMAKE_PROFILE default
