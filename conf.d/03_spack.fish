60188441f0f75531d0af66816c6ea291ce1d9894
Lucas Frérot <lucas.frerot@sorbonne-universite.fr>
2025-01-23 10:48:11 +0100

    using fixed spack version

--- conf.d/03_spack.fish
+++ conf.d/03_spack.fish
@@ -1,4 +1,4 @@
-set -x SPACK_ROOT $HOME/Documents/repos/spack
+set -x SPACK_ROOT $HOME/Documents/repos/spack-0.22.2
 if test -f $SPACK_ROOT/share/spack/setup-env.fish
     source $SPACK_ROOT/share/spack/setup-env.fish
 end
