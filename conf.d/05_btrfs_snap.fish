ed8a5fbebbafe0772caa2e00814b32f63623ade4
Lucas Frérot <lucas.frerot@sorbonne-universite.fr>
2025-01-23 10:48:25 +0100

    recursive btrfs snapshots

--- conf.d/05_btrfs_snap.fish
+++ conf.d/05_btrfs_snap.fish
@@ -1,3 +1,3 @@
 function home_snap
-    sudo btrfs subvolume snapshot /home /home/.snapshots/(date --rfc-3339=seconds | tr ' ' '_')
+    sudo btrfs subvolume snapshot -r /home /home/.snapshots/(date --rfc-3339=seconds | tr ' ' '_')
 end
